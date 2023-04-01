deee42389780cf1943c98735d61ee4e18c2294f0
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-01 05:41:51 +0000

    add uses of documents into chroma vectorstore

--- src/langchain_agent.py
+++ src/langchain_agent.py
@@ -1,3 +1,5 @@
+import os
+import json
 from pathlib import Path
 from langchain.llms import HuggingFacePipeline
 from transformers import (
@@ -9,10 +11,14 @@ from transformers import (
 )
 from langchain.embeddings import HuggingFaceEmbeddings
 
-from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+
+from langchain.vectorstores import Chroma
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
 
 # model_id = "gpt2"
 model_name = "llama-7b"
@@ -30,14 +36,20 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 # model = model.cuda()
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=50)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100)
 hf = HuggingFacePipeline(pipeline=pipe)
-embeddings = HuggingFaceEmbeddings()
+
+# using the default embedding model from hf
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
 # test embeddings
 # text = "This is a test document."
 # query_result = embeddings.embed_query(text)
 # doc_result = embeddings.embed_documents([text])
+# example_embed1 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end")
+# example_embed2 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end.")
+# from scipy.spatial import distance
+# print(f"{distance.euclidean(example_embed1, example_embed2)}")
 
 # simple text gen
 text = "Jim is a helpful business analyst that gives simple, practical answers to questions. \n Bob: What would be a good company name for a company that makes colorful socks? Give me a list of ideas. \n Jim: "
@@ -48,16 +60,76 @@ text = "Jim is a helpful business analyst that gives simple, practical answers t
 # print(f"{text_response}\n")
 # print(f"{'='*10}\n")
 
-# do document retrieval
-loader = TextLoader("./packages/langchain/docs/modules/state_of_the_union.txt")
-# loader.load()[0]
+# index documents
+loaded_doc_file = "docs/doc_loaded.json"
+doc_list = [
+    "docs/examples/state_of_the_union.txt",
+    "docs/arxiv/2302.13971.pdf",
+    # "docs/psych/DSM-5-TR.pdf",
+    # "docs/psych/Synopsis_of_Psychiatry.pdf",
+]
 
-# ask a question about the document
-index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
+
+def smart_doc_load(doc_list, loaded_doc_file):
+    loader_list = []
+    for doc in doc_list:
+        # get json record of docs loaded
+        if os.path.exists(loaded_doc_file):
+            with open(loaded_doc_file) as jfile:
+                doc_loaded = json.loads(jfile.read())
+        else:
+            doc_loaded = []
+        # check if doc has already been loaded, if so, skip
+        if doc in doc_loaded:
+            print(f"skipping {doc}")
+        else:
+            # load documents by type
+            file_type = doc.split("/")[-1].split(".")[-1]
+            if file_type == "txt":
+                loader_list.append(TextLoader(doc))
+            elif file_type == "pdf":
+                loader_list.append(UnstructuredPDFLoader(doc))
+            # save json record of docs loaded
+            print(f"loaded {doc}")
+            doc_loaded.append(doc)
+            with open(loaded_doc_file, "w") as outfile:
+                json.dump(doc_loaded, outfile)
+    return loader_list
+
+
+# https://python.langchain.com/en/latest/modules/indexes/vectorstores/examples/redis.html
+loader_list = smart_doc_load(doc_list, loaded_doc_file)
+vectorstore_kwargs = {"persist_directory": "./.chroma/persist"}
+index = VectorstoreIndexCreator(
+    embedding=embeddings, vectorstore_kwargs=vectorstore_kwargs
+).from_loaders(loader_list)
+print(f"processed {loader_list} into vector store")
+
+
+# db = Chroma(embedding_function=embeddings, **vectorstore_kwargs)
+# loaded_index = VectorStoreIndexWrapper(vectorstore=db)
+
+# query = "What did the president say about Ketanji Brown Jackson"
+# docs = db.similarity_search(query)
+
+# ask some questions about the documents
 query = "What did the president say about Ketanji Brown Jackson"
 doc_response = index.query(query, llm=hf)
-# index.query_with_sources(query, llm=hf)
-print(doc_response)
+print(f"Query - {query}\nResponse - \n{doc_response}")
+
+query = "What are some diagnostic features of substance use disorders?"
+doc_response = index.query(query, llm=hf)
+print(f"Query - {query}\nResponse - \n{doc_response}")
+
+query = "What data sources are used by llama?"
+doc_response = index.query(query, llm=hf)
+print(f"Query - {query}\nResponse - \n{doc_response}")
+
+query = "What method did llama use to tokenize its data?"
+doc_response = index.query(query, llm=hf)
+print(f"Query - {query}\nResponse - \n{doc_response}")
+
+# index.query_with_sources(query)
 
 # from langchain.chains.question_answering import load_qa_chain
 
