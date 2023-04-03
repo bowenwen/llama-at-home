353a7e05e80346b08edba9b16e5443e7fcace8f9
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-03 06:05:45 +0000

    refactoring agent and docs

--- src/my_langchain_agent.py
+++ src/my_langchain_agent.py
@@ -8,76 +8,119 @@ from transformers import (
     pipeline,
 )
 from langchain.embeddings import HuggingFaceEmbeddings
-from my_langchain_docs import MyLangchainRedisDocsHandler
+from src.my_langchain_docs import MyLangchainDocsHandler
 
 
-# model_id = "gpt2"
-model_name = "llama-7b"
-model_path = f"models/{model_name}"
+class MyLangchainAgentHandler:
+    """a wrapper to make creating a langchain agent easier"""
 
-# TODO: review config.json with model folder:
-# https://huggingface.co/docs/transformers/v4.27.2/en/internal/generation_utils#transformers.TemperatureLogitsWarper
+    def __init__(self) -> None:
+        """init function"""
+        self.model_name = None
+        self.max_new_tokens = None
+        self.model = None
+        self.tokenizer = None
+        self.embedding = None
+        self.hf = None
 
-tokenizer = AutoTokenizer.from_pretrained(Path(f"{model_path}/"))
-tokenizer.truncation_side = "left"
-model = AutoModelForCausalLM.from_pretrained(
-    Path(model_path),
-    device_map="auto",
-    quantization_config=BitsAndBytesConfig(load_in_8bit=True),
-)
-# model = model.cuda()
+    def load_llama_llm(self, model_name=None, max_new_tokens=50):
+        """quick loader of a local llama model
 
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100)
-hf = HuggingFacePipeline(pipeline=pipe)
+        Args:
+            model_name (str, optional): the model name of llama or the folder name with models folder. Defaults to None.
+            max_new_tokens (int, optional): max token size used for model. Defaults to 50.
 
-# using the default embedding model from hf
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+        Returns:model_asset_tuple: a size three tuple of HuggingFacePipeline, model and tokenizer
+        """
+        if model_name == None:
+            self.model_name = "llama-7b"
+        else:
+            self.model_name = model_name
 
-# test embeddings
-# text = "This is a test document."
-# query_result = embeddings.embed_query(text)
-# doc_result = embeddings.embed_documents([text])
-# example_embed1 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end")
-# example_embed2 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end.")
-# from scipy.spatial import distance
-# print(f"{distance.euclidean(example_embed1, example_embed2)}")
+        model_path = f"models/{self.model_name}"
+        self.max_new_tokens = max_new_tokens
 
-# simple text gen
-text = "Jim is a helpful business analyst that gives simple, practical answers to questions. \n Bob: What would be a good company name for a company that makes colorful socks? Give me a list of ideas. \n Jim: "
-# text_response = hf(text)
-# print(f"{text_response}\n")
-# print(f"{'='*10}\n")
-# text_response = hf(text)
-# print(f"{text_response}\n")
-# print(f"{'='*10}\n")
+        # TODO: review config.json with model folder:
+        # https://huggingface.co/docs/transformers/v4.27.2/en/internal/generation_utils#transformers.TemperatureLogitsWarper
 
-# query = "What did the president say about Ketanji Brown Jackson"
-# docs = db.similarity_search(query)
+        self.tokenizer = AutoTokenizer.from_pretrained(Path(f"{model_path}/"))
+        self.tokenizer.truncation_side = "left"
+        self.model = AutoModelForCausalLM.from_pretrained(
+            Path(model_path),
+            device_map="auto",
+            quantization_config=BitsAndBytesConfig(load_in_8bit=True),
+        )
+        # model = model.cuda()
+        pipe = pipeline(
+            "text-generation",
+            model=self.model,
+            tokenizer=self.tokenizer,
+            max_new_tokens=self.max_new_tokens,
+        )
+        self.hf = HuggingFacePipeline(pipeline=pipe)
+        return (self.hf, self.model, self.tokenizer)
 
-# ask some questions about the documents
-query = "What did the president say about Ketanji Brown Jackson"
-doc_response = index.query(query, llm=hf)
-print(f"Query - {query}\nResponse - \n{doc_response}")
+    def load_embedding(self):
+        """load default embedding used
 
-query = "What are some diagnostic features of substance use disorders?"
-doc_response = index.query(query, llm=hf)
-print(f"Query - {query}\nResponse - \n{doc_response}")
+        Returns:
+            HuggingFaceEmbeddings: hugging face embedding model
+        """
 
-query = "What data sources are used by llama?"
-doc_response = index.query(query, llm=hf)
-print(f"Query - {query}\nResponse - \n{doc_response}")
-
-query = "What method did llama use to tokenize its data?"
-doc_response = index.query(query, llm=hf)
-print(f"Query - {query}\nResponse - \n{doc_response}")
-
-# index.query_with_sources(query)
-
-# from langchain.chains.question_answering import load_qa_chain
-
-# chain = load_qa_chain(hf, chain_type="stuff")
-# chain.run(input_documents=docs, question=query)
+        # using the default embedding model from hf
+        self.embedding = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        )
+        return self.embedding
 
 
-# text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=100, chunk_overlap=0)
-# texts = text_splitter.split_text(state_of_the_union)
+if __name__ == "main":
+
+    # # test embeddings
+    # # text = "This is a test document."
+    # # query_result = embeddings.embed_query(text)
+    # # doc_result = embeddings.embed_documents([text])
+    # # example_embed1 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end")
+    # # example_embed2 = embeddings.embed_query("This is a very long sentence and the only difference is a period at the end.")
+    # # from scipy.spatial import distance
+    # # print(f"{distance.euclidean(example_embed1, example_embed2)}")
+
+    # # simple text gen
+    # text = "Jim is a helpful business analyst that gives simple, practical answers to questions. \n Bob: What would be a good company name for a company that makes colorful socks? Give me a list of ideas. \n Jim: "
+    # # text_response = hf(text)
+    # # print(f"{text_response}\n")
+    # # print(f"{'='*10}\n")
+    # # text_response = hf(text)
+    # # print(f"{text_response}\n")
+    # # print(f"{'='*10}\n")
+
+    # # query = "What did the president say about Ketanji Brown Jackson"
+    # # docs = db.similarity_search(query)
+
+    # # ask some questions about the documents
+    # query = "What did the president say about Ketanji Brown Jackson"
+    # doc_response = index.query(query, llm=hf)
+    # print(f"Query - {query}\nResponse - \n{doc_response}")
+
+    # query = "What are some diagnostic features of substance use disorders?"
+    # doc_response = index.query(query, llm=hf)
+    # print(f"Query - {query}\nResponse - \n{doc_response}")
+
+    # query = "What data sources are used by llama?"
+    # doc_response = index.query(query, llm=hf)
+    # print(f"Query - {query}\nResponse - \n{doc_response}")
+
+    # query = "What method did llama use to tokenize its data?"
+    # doc_response = index.query(query, llm=hf)
+    # print(f"Query - {query}\nResponse - \n{doc_response}")
+
+    # # index.query_with_sources(query)
+
+    # # from langchain.chains.question_answering import load_qa_chain
+
+    # # chain = load_qa_chain(hf, chain_type="stuff")
+    # # chain.run(input_documents=docs, question=query)
+
+    # # text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=100, chunk_overlap=0)
+    # # texts = text_splitter.split_text(state_of_the_union)
+    pass
--- src/my_langchain_docs.py
+++ src/my_langchain_docs.py
@@ -16,21 +16,36 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.redis import Redis
 
 
-class MyLangchainRedisDocsHandler:
+class MyLangchainDocsHandler:
     """a wrapper to make loading my documents easier"""
 
     DEFAULT_DOMAIN = "localhost"
 
-    def connect_to_redis():
-        pass
+    def __init__(self, embeddings, redis_host=DEFAULT_DOMAIN):
+        self.embeddings = embeddings
+        self.redis_host = redis_host
 
-    def load_docs_into_redis(
-        self, embeddings, loaded_doc_file, doc_list, redis_host=DEFAULT_DOMAIN
-    ):
+    def index_from_redis(self, index_name):
+        rds = Redis.from_existing_index(
+            self.embeddings,
+            redis_url=f"redis://{self.redis_host}:6379",
+            index_name=index_name,
+        )
+        return rds
 
-        # https://python.langchain.com/en/latest/modules/indexes/vectorstores/examples/redis.html
+    def load_docs_into_chroma(self, doc_list, loaded_doc_file):
         loader_list = self.smart_generate_doc_loaders(doc_list, loaded_doc_file)
         vectorstore_kwargs = {"persist_directory": "./.chroma/persist"}
+
+        index = VectorstoreIndexCreator(
+            embedding=self.embeddings, vectorstore_kwargs=vectorstore_kwargs
+        ).from_loaders(loader_list)
+
+        return index
+
+    def load_docs_into_redis(self, doc_list, loaded_doc_file):
+
+        # https://python.langchain.com/en/latest/modules/indexes/vectorstores/examples/redis.html
 
         loader = TextLoader("../../../state_of_the_union.txt")
         documents = loader.load()
@@ -38,16 +53,15 @@ class MyLangchainRedisDocsHandler:
         docs = text_splitter.split_documents(documents)
 
         rds = Redis.from_documents(
-            docs, embeddings, redis_url=f"redis://{redis_host}:6379", index_name="link"
+            docs,
+            self.embeddings,
+            redis_url=f"redis://{self.redis_host}:6379",
+            index_name="link",
         )
         # rds.index_name
         query = "What did the president say about Ketanji Brown Jackson"
         results = rds.similarity_search(query)
         print(results[0].page_content)
-
-        # index = VectorstoreIndexCreator(
-        #     embedding=embeddings, vectorstore_kwargs=vectorstore_kwargs
-        # ).from_loaders(loader_list)
         print(f"processed {loader_list} into vector store")
 
     @staticmethod
@@ -56,7 +70,7 @@ class MyLangchainRedisDocsHandler:
         for doc in doc_list:
             # get json record of docs loaded
             if os.path.exists(loaded_doc_file):
-                with open(loaded_doc_file) as jfile:
+                with open(loaded_doc_file, "r") as jfile:
                     doc_loaded = json.loads(jfile.read())
             else:
                 doc_loaded = []
@@ -79,9 +93,6 @@ class MyLangchainRedisDocsHandler:
 
 
 if __name__ == "main":
-    embedding = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-mpnet-base-v2"
-    )
 
     # index documents
     loaded_doc_file = "docs/doc_loaded.json"
@@ -91,10 +102,17 @@ if __name__ == "main":
         # "docs/psych/DSM-5-TR.pdf",
         # "docs/psych/Synopsis_of_Psychiatry.pdf",
     ]
-    tester = MyLangchainRedisDocsHandler()
-    tester.load_docs_into_redis(
-        loaded_doc_file=loaded_doc_file,
-        doc_list=doc_list,
-        redis_host="192.168.1.236",
-        embeddings=embedding,
+    tester = MyLangchainDocsHandler(embeddings=embedding, redis_host="192.168.1.236")
+    # index = tester.load_docs_into_redis(
+    #     doc_list=doc_list,
+    #     loaded_doc_file=loaded_doc_file,
+    #     embeddings=embedding,
+    #
+    # )
+    index = tester.load_docs_into_chroma(
+        doc_list=doc_list, loaded_doc_file=loaded_doc_file
     )
+    query = "What did the president say about Ketanji Brown Jackson"
+    doc_response = index.query(query, llm=hf)
+    print(f"Query - {query}\nResponse - \n{doc_response}")
+    pass
