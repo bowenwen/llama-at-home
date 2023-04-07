25c00657a4c6b3dbec86d9895a2f296043c73b7b
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-07 19:55:42 +0000

    fix bug and add master file list

--- src/my_langchain_run.py
+++ src/my_langchain_run.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append("./")
+
 from scipy.spatial import distance
 
 from src.my_langchain_agent import MyLangchainAgentHandler
@@ -9,10 +13,7 @@ embedding = testAgent.load_hf_embedding()
 text1 = "This is a very long sentence and the only difference is a period at the end"
 text2 = "This is a very long sentence and the only difference is a period at the end."
 query_result1 = embedding.embed_query(text1)
-print(f"text1 result:\n{str(query_result1[0:5]).replace(']','...')}")
 query_result2 = embedding.embed_query(text2)
-print(f"text2 result:\n{str(query_result2[0:5]).replace(']','...')}")
-
 print(f"text1 and text2 distance: {distance.euclidean(query_result1, query_result2)}")
 doc_result = embedding.embed_documents([text1, text2])
 print(f"text1 within doc:\n{str(doc_result[0][0:5]).replace(']','...')}")
@@ -24,21 +25,24 @@ hf, model, tokenizer = testAgent.load_llama_llm(
 )
 
 # index documents
-# index_name = "examples"
-# index_name = "arxiv"
-index_name = "psych"
-file_list = [
-    # "index-docs/examples/state_of_the_union.txt",
-    # "index-docs/arxiv/2302.13971.pdf",
-    "index-docs/psych/DSM-5-TR.pdf",
-    "index-docs/psych/Synopsis_of_Psychiatry.pdf",
-]
+index_name = "examples"
+
+file_list_master = {
+    "examples": ["index-docs/examples/state_of_the_union.txt"],
+    "arxiv": ["index-docs/arxiv/2302.13971.pdf"],
+    "psych_dsm": ["index-docs/psych/DSM-5-TR.pdf"],
+    "psych_sop": ["index-docs/psych/Synopsis_of_Psychiatry.pdf"],
+}
+file_list = file_list_master[index_name]
+
 testDocs = MyLangchainDocsHandler(embedding=embedding, redis_host="192.168.1.236")
 # index = testDocs.load_docs_into_chroma(file_list, index_name)
 index = testDocs.load_docs_into_redis(file_list, index_name)
 
 # using the VectorStoreIndexWrapper to run a chain with question related to the doc
-index.similarity_search("What did the president say about Ketanji Brown Jackson")
+index.vectorstore.similarity_search(
+    "What did the president say about Ketanji Brown Jackson"
+)
 query = "What did the president say about Ketanji Brown Jackson"
 doc_response = index.query(query, llm=hf)
 print(f"Query - {query}\nResponse - \n{doc_response}")
