dffbc3c72bcbfb8c7e6c45ffef4d4450ba001d37
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-13 04:23:57 +0000

    add google api and wiki to agent executor

--- src/my_langchain_agent_executor.py
+++ src/my_langchain_agent_executor.py
@@ -7,12 +7,15 @@ import warnings
 
 sys.path.append("./")
 
-from langchain.utilities import WikipediaAPIWrapper
+from langchain.utilities import (
+    WikipediaAPIWrapper,
+    SerpAPIWrapper,
+    GoogleSearchAPIWrapper,
+)
 from langchain.chains import ConversationChain, RetrievalQA
 from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
 from langchain.prompts.prompt import PromptTemplate
-from langchain.agents import initialize_agent, Tool
-from langchain.agents import AgentType
+from langchain.agents import initialize_agent, Tool, AgentType
 from langchain.tools import BaseTool
 from src.my_langchain_agent import MyLangchainAgentHandler
 from src.my_langchain_docs import MyLangchainDocsHandler
@@ -21,6 +24,7 @@ from src.my_langchain_docs import MyLangchainDocsHandler
 warnings.filterwarnings("ignore")
 os.environ["PYDEVD_INTERRUPT_THREAD_TIMEOUT"] = "60"
 os.environ["PYDEVD_WARN_EVALUATION_TIMEOUT "] = "60"
+
 
 # set up log
 # targets = logging.StreamHandler(sys.stdout), logging.FileHandler("test.log")
@@ -31,12 +35,13 @@ os.environ["PYDEVD_WARN_EVALUATION_TIMEOUT "] = "60"
 
 # select model and lora
 model_name = "llama-13b"
+# model_name = "llama-30b-4bit-128g"
 lora_name = "alpaca-lora-13b"
 
 testAgent = MyLangchainAgentHandler()
 embedding = testAgent.load_hf_embedding()
 hf, model, tokenizer = testAgent.load_llama_llm(
-    model_name=model_name, lora_name=lora_name, max_new_tokens=500
+    model_name=model_name, lora_name=lora_name, max_new_tokens=50
 )
 
 # start simple gradio app
@@ -64,6 +69,38 @@ state_of_union = RetrievalQA.from_chain_type(
 
 wikipedia = WikipediaAPIWrapper()
 
+
+def truncate_wikipedia(input_string):
+    full_string = wikipedia.run(input_string)
+    truncated_string = full_string[0:1000]
+    return truncated_string
+
+
+def summarize_wikipedia(input_string):
+    # use LLM to summarize the meaning of the Wikipedia text
+    return None
+
+
+def get_secrets(key_name):
+    _key_file = open(f"secrets/{key_name}.key", "r", encoding="utf-8")
+    _key_value = _key_file.read()
+    return _key_value
+
+
+# Search
+# Serp API
+# os.environ["SERPAPI_API_KEY"] = get_secrets("serpapi")
+# search = SerpAPIWrapper()
+
+# Google API
+os.environ["GOOGLE_API_KEY"] = get_secrets("googleapi")
+os.environ["GOOGLE_CSE_ID"] = get_secrets("googlecse")
+search = GoogleSearchAPIWrapper()
+
+# Try this instead:
+# Google search
+# https://python.langchain.com/en/latest/modules/agents/tools/examples/google_search.html
+
 tools = [
     Tool(
         name="State of Union Document",
@@ -72,8 +109,18 @@ tools = [
     ),
     Tool(
         name="Wikipedia",
-        func=wikipedia.run,
+        func=truncate_wikipedia,  # replacing wikipedia.run
         description="useful for when you need to find facts and information from Wikipedia.",
+    ),
+    Tool(
+        name="Search",
+        func=search.run,
+        description="useful for when you need to find facts and information from Google.",
+    ),
+    Tool(
+        name="Google",
+        func=search.run,
+        description="useful for when you need to find facts and information from Google.",
     ),
 ]
 
@@ -94,7 +141,7 @@ tools = [
 
 # main_prompt = """What year did Russia annexed Crimea? Did the president mention this historical event in the state of the union?"""
 
-main_prompt = "What former president of the United States been charged with crimes?"
+main_prompt = "Create a summary of current events and highlight ones related to the Economy."
 
 agent = initialize_agent(
     tools, hf, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
