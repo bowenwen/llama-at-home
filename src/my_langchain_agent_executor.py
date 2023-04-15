3aba8f2ca3ef008c70c2f28ffa0338e74e795d0e
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-15 07:19:03 +0000

    add searx search and wiki summary functions

--- src/my_langchain_agent_executor.py
+++ src/my_langchain_agent_executor.py
@@ -1,3 +1,5 @@
+# %%
+
 # Goal: Agent with doc search
 
 import logging
@@ -5,12 +7,14 @@ import sys
 import os
 import warnings
 
+os.chdir("/home/coder/projects/llama-at-home")
 sys.path.append("./")
 
 from langchain.utilities import (
     WikipediaAPIWrapper,
     SerpAPIWrapper,
     GoogleSearchAPIWrapper,
+    SearxSearchWrapper,
 )
 from langchain.chains import ConversationChain, RetrievalQA
 from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
@@ -36,7 +40,9 @@ os.environ["PYDEVD_WARN_EVALUATION_TIMEOUT "] = "60"
 # select model and lora
 model_name = "llama-13b"
 # model_name = "llama-30b-4bit-128g"
-lora_name = "alpaca-lora-13b"
+# lora_name = "alpaca-lora-13b"
+# lora_name = "alpaca-gpt4-lora-13b"
+lora_name = "alpaca-gpt4-lora-13b-3ep"
 
 testAgent = MyLangchainAgentHandler()
 embedding = testAgent.load_hf_embedding()
@@ -44,6 +50,7 @@ hf, model, tokenizer = testAgent.load_llama_llm(
     model_name=model_name, lora_name=lora_name, max_new_tokens=200
 )
 
+# %%
 # start simple gradio app
 import gradio as gr
 
@@ -67,18 +74,22 @@ state_of_union = RetrievalQA.from_chain_type(
     llm=hf, chain_type="stuff", retriever=vectorstore_retriever
 )
 
-wikipedia = WikipediaAPIWrapper()
+# %%
+wikipedia = WikipediaAPIWrapper(top_k_results=1)
 
 
 def truncate_wikipedia(input_string):
     full_string = wikipedia.run(input_string)
-    truncated_string = full_string[0:1000]
+    truncated_string = full_string[0:2000]
     return truncated_string
 
 
 def summarize_wikipedia(input_string):
     # use LLM to summarize the meaning of the Wikipedia text
-    return None
+    full_string = wikipedia.run(input_string)
+    summarize_prompt = f"### Instruction: Please provide a detailed summary of the following information \n### Input:\n{full_string} \n### Response: "
+    summarized_text = hf(summarize_prompt)
+    return summarized_text
 
 
 def get_secrets(key_name):
@@ -87,69 +98,91 @@ def get_secrets(key_name):
     return _key_value
 
 
-# Search
-# Serp API
+# # Search
+# # Serp API
 # os.environ["SERPAPI_API_KEY"] = get_secrets("serpapi")
 # search = SerpAPIWrapper()
 
-# Google API
-os.environ["GOOGLE_API_KEY"] = get_secrets("googleapi")
-os.environ["GOOGLE_CSE_ID"] = get_secrets("googlecse")
-search = GoogleSearchAPIWrapper()
+# # Google API
+# os.environ["GOOGLE_API_KEY"] = get_secrets("google2api")
+# os.environ["GOOGLE_CSE_ID"] = get_secrets("google2cse")
+# search = GoogleSearchAPIWrapper(k=3)  # top k results only
 
+# Searx API
+# https://python.langchain.com/en/latest/modules/agents/tools/examples/searx_search.html
+ssearch = SearxSearchWrapper(
+    searx_host=get_secrets("searx_host"), k=3, engines=["google"]
+)
+
+
+def searx_google_search(input_string):
+    # reference: https://searx.github.io/searx/admin/engines.html
+    # https://python.langchain.com/en/latest/modules/indexes/document_loaders/examples/web_base.html
+    truncated_string = ""
+    while truncated_string == "":
+        try:
+            truncated_string = ssearch.run(input_string).replace("\n", " ")
+        except:
+            truncated_string = ""
+    return truncated_string
+
+
+# %%
 # Try this instead:
 # Google search
 # https://python.langchain.com/en/latest/modules/agents/tools/examples/google_search.html
 
 tools = [
-    Tool(
-        name="State of Union Document",
-        func=state_of_union.run,
-        description="contains the recent state of the union address",
-    ),
+    # Tool(
+    #     name="State of Union QA System",
+    #     func=state_of_union.run,
+    #     description="specific facts from the 2023 state of the union on Joe Biden's plan to rebuild the economy and unite the nation.",
+    # ),
     Tool(
         name="Wikipedia",
-        func=truncate_wikipedia,  # replacing wikipedia.run
-        description="useful for when you need to find facts and information from Wikipedia.",
+        func=summarize_wikipedia,
+        # func=truncate_wikipedia,
+        description="general information and well-established facts on a topic.",
     ),
-    Tool(
-        name="Search",
-        func=search.run,
-        description="useful for when you need to find facts and information from Google.",
-    ),
+    # Tool(
+    #     name="Google",
+    #     func=search.run,
+    #     description="recent events and specific facts about a topic.",
+    # ),
     Tool(
         name="Google",
-        func=search.run,
-        description="useful for when you need to find facts and information from Google.",
+        func=searx_google_search,
+        description="recent events and specific facts about a topic.",
     ),
 ]
 
-# tools = [
-#     Tool(
-#         name = "Intermediate Answer",
-#         func=wikipedia.run,
-#         description="useful for when you need to find information from Wikipedia."
-#     )
-# ]
+# enter prompt
+# main_prompt = """What is the current inflation rate in the United States?"""
+# main_prompt = """Who leaked the document on Ukraine?"""
+# main_prompt = """Which city will be hosting the summer olympics in 2036?"""
+# main_prompt = """Which city will be hosting the summer olympics in 2032?"""
+# main_prompt = """What is the current progress on nuclear fusion?"""
+# main_prompt = """What wars are happening around the world right now?"""
+main_prompt = """Summarize the current events today on the US Economy."""
+# main_prompt = """What is the current financial situation of TransLink?"""
+print(
+    "\x1b[1;32m"
+    f"""\n\nQuestion: {main_prompt}
+Thought:"""
+    + "\x1b[0m"
+)
 
-# NOT WORKING:
-## problem is that it is not properly calling the tools
-## ideas to try:
-## different AgentType?
-## go back to examples?
-## manually format things?
-
-# main_prompt = """What year did Russia annexed Crimea? Did the president mention this historical event in the state of the union?"""
-
-main_prompt = "Create a summary of current events and highlight ones related to the Economy."
-
+# run agent executor chain
 agent = initialize_agent(
     tools, hf, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
-agent.run(main_prompt)
+result = agent.run(main_prompt)
+print(result)
 
+# exit
 print("done")
 
+# %%
 # def question_answer(question, not_used):
 #     template = """Below is an instruction that describes a task.
 #     {history}
